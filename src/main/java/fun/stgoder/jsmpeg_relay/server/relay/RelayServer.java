5b575634f36fdb9e1befc9b366627eed0a684e75
stgoder@163.com <stgoder@163.com>
2019-12-29 01:48:22 +0800

    epoll support

--- src/main/java/fun/stgoder/jsmpeg_relay/server/relay/RelayServer.java
+++ src/main/java/fun/stgoder/jsmpeg_relay/server/relay/RelayServer.java
@@ -3,19 +3,34 @@ package fun.stgoder.jsmpeg_relay.server.relay;
 import fun.stgoder.jsmpeg_relay.common.Constants;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 public class RelayServer {
-    private EventLoopGroup bossGroup = new NioEventLoopGroup();
-    private EventLoopGroup wokerGroup = new NioEventLoopGroup();
-    private ServerBootstrap serverBootstrap = new ServerBootstrap();
+    private final EventLoopGroup bossGroup;
+    private final EventLoopGroup wokerGroup;
+    private final ServerBootstrap serverBootstrap;
     private int port;
 
+    {
+        if (Epoll.isAvailable()) {
+            bossGroup = new EpollEventLoopGroup();
+            wokerGroup = new EpollEventLoopGroup();
+        } else {
+            bossGroup = new NioEventLoopGroup();
+            wokerGroup = new NioEventLoopGroup();
+        }
+        serverBootstrap = new ServerBootstrap();
+    }
+
     public RelayServer(int port) {
-        serverBootstrap.group(bossGroup, wokerGroup).channel(NioServerSocketChannel.class)
+        serverBootstrap.group(bossGroup, wokerGroup)
+                .channel(Epoll.isAvailable() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
                 .handler(new LoggingHandler(LogLevel.INFO))
                 .childHandler(new RelayChannelInitializer());
         this.port = port;
